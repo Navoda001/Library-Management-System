1f48c65f5143e68478b7225ab2a93c80dfba278e
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-02-22 20:57:02 +0530

    create project

---
+++ src/test/java/com/library/LibMgmt2025/LibMgmt2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.LibMgmt2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibMgmt2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
